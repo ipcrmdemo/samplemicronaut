270ce3ff7a67b7ded97e1de40e008fa87e72e51e
Lieven Doclo <lieven@atomist.com>
2019-04-16 15:22:34 +0200

    Add home controller

---
+++ src/main/java/com/atomist/HomeController.java
@@ -0,0 +1,15 @@
+package com.atomist;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+@Controller("/") 
+public class HomeController {
+    @Get("/") 
+    @Produces(MediaType.TEXT_PLAIN) 
+    public String index() {
+        return "Hello World"; 
+    }
+}
